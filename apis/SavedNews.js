041a0f2b05f97349653fc4744a6885b1da8f09b6
allenliu123 <973400732@qq.com>
2020-04-03 00:05:48 +0800

    first commit

---
+++ apis/SavedNews.js
@@ -0,0 +1,42 @@
+/**
+ * 用于保存 10 条新闻
+ */
+class SavedNews10 {
+
+  // 这是一个单例类
+  constructor() {
+    if (!SavedNews10.instance) {
+      this.newsList = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+      SavedNews10.instance = this;
+    }
+    return SavedNews10.instance;
+  }
+
+  // 列表头部加入一条最新新闻，并删除最后一条，保持最新的 10 条新闻
+  push(news) {
+    this.newsList.unshift(news);
+    this.newsList.length = 10;
+  }
+
+  // 返回保存的 10 条新闻
+  get() {
+    return this.newsList;
+  }
+
+  /**
+   * 对比网站上爬取的最新新闻，返回有几条不同 
+   * @param { List } nowNewsList 网站上爬取的最新新闻
+   * @return { Number } 返回几，就表示需要插入几条数据，返回 -1 表示没有相同的，需要全部插入
+   */
+  compare(nowNewsList) {
+    for(let index = 0; index < nowNewsList.length; index++) {
+      if(this.newsList[0].url === nowNewsList[index].url
+      && this.newsList[0].title === nowNewsList[index].title) {
+        return index;
+      }
+    }
+    return 10;
+  }
+}
+
+module.exports = SavedNews10;
---
+++ apis/index.js
@@ -0,0 +1,45 @@
+let request = require('sync-request');
+let cheerio = require('cheerio');
+
+// 爬取 10 条最新新闻
+function crawlNowNews10() {
+  // 时政要闻网页
+  let url = 'http://news.cqjtu.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1021';
+  let html = request('get', url, {
+    headers: {
+      'User-Agent': 'Request-Promise'
+    }
+  }).getBody().toString();
+  let $ = cheerio.load(html);
+  // 找到网页上的“最新新闻“版块
+  let nowNewsList = $('.zxxw>li');
+  let returnList= [];
+  for(let i = 0; i < 10; i++) {
+    let obj = new Object();
+    obj.url = 'http://news.cqjtu.edu.cn' + nowNewsList[i].children[0].attribs.href;
+    obj.title = nowNewsList[i].children[0].attribs.title;
+    returnList.push(obj);
+  }
+  return returnList;
+  // console.log($('.zxxw>li')[0].children[0].attribs.href);
+}
+
+// 爬取 新闻详情
+function crawlNewDetail(url) {
+  let html = request('get', url, {
+    headers: {
+      'User-Agent': 'Request-Promise'
+    }
+  }).getBody().toString();
+  let $ = cheerio.load(html);
+  return {
+    type: $('.currt-nav')[0].children[3].children[0].data || '',
+    time: $('.article-editor p')[0].children[2].data.trim() || '',
+    content: 'content'
+  }
+}
+
+module.exports = {
+  crawlNowNews10: crawlNowNews10,
+  crawlNewDetail: crawlNewDetail
+}
